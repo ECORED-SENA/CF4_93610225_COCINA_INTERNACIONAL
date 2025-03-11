ff239969005645d90a1044756ecb29be66f4daf0
Leyson Castaño <leysofcp@hotmail.com>
2025-03-10 21:52:14 -0500

    carga de PDF accesible y zip

--- src/config/global.js
+++ src/config/global.js
@@ -83,7 +83,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/93610225_CF04_DU.pdf',
       },
       {
         icono: 'fas fa-download',
